0a72ccb9fff4e838756232ac47709b280f462a70
Gabriel Pagella <gabriel.pagella@kinandcarta.com>
2023-03-20 12:25:04 -0300

    Shopping basket and tests developed using TDD

---
+++ src/Item.java
@@ -0,0 +1,13 @@
+public class Item {
+    private double unitPrice;
+    private int quantity;
+
+    public Item(double unitPrice, int quantity) {
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+
+    public double getSubtotal() {
+        return unitPrice * (double) quantity;
+    }
+}
---
+++ src/ShoppingBasket.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class ShoppingBasket {
+    private final List<Item> items;
+
+    public ShoppingBasket(List<Item> items) {
+        this.items = items;
+    }
+
+    public double getTotal() {
+        return items.stream().mapToDouble(item -> item.getSubtotal()).sum();
+    }
+}
